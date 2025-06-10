99819cbaae91604c07398e529e779e03aff11d23
Legume Abi <147527392+legumeabi@users.noreply.github.com>
2025-06-11 01:10:43 +0200

    Add singlefile bundling plugin and use it

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [viteSingleFile()],
   build: {
     outDir: "build",
   },
