b3bf89562bd6a4ba0eccbed8ac7d072584f0a6b3
Legume Abi <147527392+legumeabi@users.noreply.github.com>
2025-06-14 23:12:39 +0200

    Fix tests

---
+++ src/overlay/test/setup.js
@@ -0,0 +1,8 @@
+// Define Vite's build-time variables for tests
+globalThis.__CHANNEL_NAME__ = "test_channel";
+globalThis.__CHAT_STRIP_MODE__ = false;
+globalThis.__MAX_MESSAGES__ = 20;
+globalThis.__SHOW_BADGES__ = false;
+globalThis.__SHOW_PRONOUNS__ = true;
+globalThis.__REMOVE_MESSAGES_AFTER_TIMER__ = false;
+globalThis.__MESSAGE_REMOVAL_TIMER__ = 5000;
